5d075246e7b2123de7086d09c4c6d70951753aa7
CyberKitsune <cyberkitsune09@gmail.com>
2022-08-28 19:49:22 -0700

    Runscripts: Add runscripts

---
+++ RunVRCNowPlaying.bat
@@ -0,0 +1,3 @@
+echo "Installing requirements (be sure to have python installed and in PATH)"
+pip install -r VRCNowPlaying/Requirements.txt
+python VRCNowPlaying/vrcnowplaying.py
---
+++ RunVRCSubs.bat
@@ -0,0 +1,3 @@
+echo "Installing requirements (be sure to have python installed and in PATH)"
+pip install -r VRCSubs/Requirements.txt
+python VRCSubs/vrcsubs.py
