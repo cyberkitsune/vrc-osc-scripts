72ce2813d725d84b220594053505367caeb169c8
CyberKitsune <cyberkitsune09@gmail.com>
2022-11-03 14:32:46 -0700

    Updatecheck: Add update checker

--- RunVRCNowPlaying.bat
+++ RunVRCNowPlaying.bat
@@ -1,4 +1,5 @@
 @echo off
+call UpdateScripts.bat
 echo "Installing requirements (be sure to have python installed and in PATH)"
 pip install -r VRCNowPlaying/Requirements.txt
 python VRCNowPlaying/vrcnowplaying.py
---
+++ UpdateScripts.bat
@@ -0,0 +1,4 @@
+@echo off
+echo "Checking for updates..."
+pip install -r Requirements.txt
+python Updatecheck.py
