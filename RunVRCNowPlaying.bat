f98e653661ec114d176e8bc50565c7d5b7122a24
CyberKitsune <cyberkitsune09@gmail.com>
2023-01-16 23:51:48 -0800

    Runscripts: Explicitly use 'python -m pip' instead of pip
    This HOPEFULLY should fix weird edge cases when using multiple python
    installs where, for some reason, the 'pip' binary in PATH is NOT the
    'pip' binary belonging to whatever 'python' is in PATH
    
    How does this even work on Windows... lmao

--- RunVRCNowPlaying.bat
+++ RunVRCNowPlaying.bat
@@ -1,6 +1,6 @@
 @echo off
 call UpdateScripts.bat
 echo "Installing requirements (be sure to have python installed and in PATH)"
-pip install -r VRCNowPlaying/Requirements.txt
+python -m pip install -r VRCNowPlaying/Requirements.txt
 python VRCNowPlaying/vrcnowplaying.py
 pause
--- UpdateScripts.bat
+++ UpdateScripts.bat
@@ -1,4 +1,4 @@
 @echo off
 echo "Checking for updates..."
-pip install -r Requirements.txt
+python -m pip install -r Requirements.txt
 python Updatecheck.py
