1418c003984a0e442fe27fbc79783062925addab
CyberKitsune <cyberkitsune09@gmail.com>
2023-04-08 17:20:29 -0700

    CheckPython: Handle winget errors better

--- CheckPython.bat
+++ CheckPython.bat
@@ -10,7 +10,14 @@ IF !ERRORLEVEL! NEQ 0 (
     ) ELSE (
         choice /C:yn /m "You do not have Python installed. Do you wish to install it? [Y] / [N]"
         if !ERRORLEVEL! == 1 (
+            echo "Please accept any licence agreements after this to install python by pressing Y then enter."
             winget install python
+            if !ERRORLEVEL! NEQ 0 (
+                echo "For some reason Python failed to install with winget, install Python manually with the windows store or using python.org"
+                echo "Feel free to contact the vrc-osc-scripts Discord for more help"
+                pause
+                exit
+            )
         )
     )
 ) ELSE (
