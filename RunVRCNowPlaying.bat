4ddb0368af1e38dec015297f32ff643491fa4219
Jamie Slyman <wdcook4@gmail.com>
2023-04-26 22:55:51 -0400

    Make logging format consistent, de-clutter.

--- RunVRCNowPlaying.bat
+++ RunVRCNowPlaying.bat
@@ -1,6 +1,6 @@
 @echo off
 call UpdateScripts.bat
-echo "Installing requirements (be sure to have python installed and in PATH)"
-python -m pip install -r VRCNowPlaying/Requirements.txt
+echo [%~n0] Installing requirements (be sure to have python installed and in PATH)
+python -m pip install -r VRCNowPlaying/Requirements.txt -q
 python VRCNowPlaying/vrcnowplaying.py
 pause
--- RunVRCSubs.bat
+++ RunVRCSubs.bat
@@ -1,6 +1,6 @@
 @echo off
 call UpdateScripts.bat
-echo "Installing requirements (be sure to have python installed and in PATH)"
-python -m pip install -r VRCSubs/Requirements.txt
+echo [%~n0] Installing requirements (be sure to have python installed and in PATH)
+python -m pip install -r VRCSubs/Requirements.txt -q
 python VRCSubs/vrcsubs.py
 pause
