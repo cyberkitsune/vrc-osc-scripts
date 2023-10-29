12fe282f462fff7ab947224e8c150ff647fb7054
CyberKitsune <920550+cyberkitsune@users.noreply.github.com>
2023-10-28 18:17:17 -0700

    Update RunVRCSubs.bat
    Add pip update

--- RunVRCSubs.bat
+++ RunVRCSubs.bat
@@ -1,6 +1,7 @@
 @echo off
 call UpdateScripts.bat
 echo [%~n0] Installing requirements (be sure to have python installed and in PATH)
+python -m pip install --upgrade pip
 python -m pip install -r VRCSubs/Requirements.txt -q
 python VRCSubs/vrcsubs.py
 pause
