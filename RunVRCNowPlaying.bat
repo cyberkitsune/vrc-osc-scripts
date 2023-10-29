126a0ddff52974c0bbf8586c9ef0ec8253b18c64
CyberKitsune <920550+cyberkitsune@users.noreply.github.com>
2023-10-28 18:17:37 -0700

    Update RunVRCNowPlaying.bat
    Add pip update

--- RunVRCNowPlaying.bat
+++ RunVRCNowPlaying.bat
@@ -1,6 +1,7 @@
 @echo off
 call UpdateScripts.bat
 echo [%~n0] Installing requirements (be sure to have python installed and in PATH)
+python -m pip install --upgrade pip
 python -m pip install -r VRCNowPlaying/Requirements.txt -q
 python VRCNowPlaying/vrcnowplaying.py
 pause
